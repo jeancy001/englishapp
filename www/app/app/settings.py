5c1bd156b38987bf60f4b021efe224421d14c583
jeancy001 <jeancympoy24@gmail.com>
2024-03-28 20:21:03 +0200

    files added

--- www/app/app/settings.py
+++ www/app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rb9mgyowe4)1bm-_qd2=0(!#$a!*!jh&2wrhyse+nr%mz-+qj+'
+SECRET_KEY = 'django-insecure-=si37a@n)m**u3yrccvdq41%0w2ld%pr&md#hllh%9z!s4hs4w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
