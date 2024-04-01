70cf1424282e11d4c6eef3c4f5127554e6332b31
jeancy001 <jeancympoy24@gmail.com>
2024-04-01 15:30:27 +0200

    added

--- www/app/app/settings.py
+++ www/app/app/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
--- www/app/englishroom/views.py
+++ www/app/englishroom/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from  app import settings
+from app import settings
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login, logout
 from django.contrib import messages 
@@ -46,7 +46,9 @@ def register(request):
     if len(lastname)<=3:
       messages.error(request, "Write a correct Last name ")
       return redirect("englishroom:register")
-    
+    if User.objects.filter(password = password):
+      messages.error(request, "This password is not Strong ,Must container Upper and lowercase ")
+      return redirect("englishroom:register")
     if password != password1 :
       messages.error(request, "Password must match .")
       return redirect("englishroom:register")
@@ -86,7 +88,7 @@ def register(request):
       #send email
       send_mail (subject, message, from_to , to_all_users, fail_silently=False)
       
-      return redirect ("englishroom:user_login")
+      return redirect ("englishroom:/")
 
       
   
@@ -95,7 +97,35 @@ def register(request):
 
 #create a user_login funtions 
 def user_login(request): 
-  
+  if request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+    if len(username) and len(password) <=0:
+      messages.error(request, "Fill out your Username or Password  to continue ")
+      return redirect("enlgishroo:user_login")
+    user  = authenticate(request,username = username, password =password)
+    
+    if user is not None :
+      login(request, user) 
+      username = user.username 
+      messages.success(request, "Login successfully !")
+      #send message to confirm the user login . 
+      
+      subject ="Confirm  Login"
+      message = f"""
+      Hey dear {user.username}! 
+      welcome to englishroom  . 
+      \n Thank you for your  trust .
+      www.engishroom.com
+      """
+      form_to =settings.EMAIL_HOST_USER 
+      to_all_connected = [user.email]
+      send_mail (subject,message, form_to, to_all_connected, fail_silently= False )
+      return redirect ("englishroom:home")
+    else:
+      messages.error(request, "Login failed, use a correct ID  ")
+      return redirect("englishroom:user_login")
+    
   return render (request, "englishroom/user_login.html")
 
 
@@ -106,3 +136,8 @@ def user_login(request):
 def welcome_user(request): 
   
   return render (request, "englishroom/welcome_user.html")
+
+#logout users 
+def user_logout(request):
+  logout(request)
+  return HttpResponseRedirect(reverse("englishroom:user_login"))
